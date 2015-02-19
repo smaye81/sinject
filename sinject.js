0724750af2ea28513f49b1c34e6d2eaf0795f1e8
Steve Ayers <smaye81@gmail.com>
2015-02-18 23:39:18 -0500

    Added support for ES6

--- sinject.js
+++ sinject.js
@@ -1,78 +1,67 @@
-(function (root, factory) {
-  if (typeof define === "function" && define.amd) {
-    define([], factory);
-  } else if (typeof exports === "object") {
-    module.exports = factory();
-  } else {
-    root.Requester = factory();
-  }
-}(this, function () {
+function SInject() {
+  var objMap = {}; // Would use a Map in ES6 so token can be anything
+  var registered = [];
+  var debug = false;
+  var bootstrapped = false;
 
-  function SInject() {
-    var objMap = {}; // Would use a Map in ES6 so token can be anything
-    var registered = [];
-    var debug = false;
-    var bootstrapped = false;
-
-    function enableDebugging() {
-      debug = true;
-    }
-
-    function getObject(token) {
-      if (debug && bootstrapped) {
-        if (!objMap.hasOwnProperty(token)) {
-          console.warn('Token `' + token + '` was requested, but was never provided.');
-        }
-      }
-      return objMap[token];
-    }
-
-    function provide(token, obj) {
-      if (debug && objMap.hasOwnProperty(token)) {
-        console.warn('Token `' + token + '` is being re-defined, from `' + objMap[token] + '` to `' + obj + '`.');
-      }
-      objMap[token] = obj;
-    }
-
-    function inject(fn/*, tokens */) {
-      var tokens = [].slice.call(arguments, 1);
-      var fnDescription = {tokens: tokens, fn: fn};
-      if (bootstrapped) {
-        performInject(fnDescription);
-      } else {
-        registered.push(fnDescription);
-      }
-    }
-
-    function register(token, obj, fn/*, tokens */) {
-      var tokens = [].slice.call(arguments, 3);
-      provide(token, obj);
-      inject(fn, tokens);
-    }
-
-    function performInject(fnDescription) {
-      fnDescription.fn.apply(undefined, fnDescription.tokens && fnDescription.tokens.map(getObject));
-    }
-
-    function bootstrap() {
-      registered.forEach(performInject);
-      registered = [];
-      bootstrapped = true;
-    }
-
-    return exports = {
-      provide:         provide,
-      inject:          inject,
-      register:        register,
-      bootstrap:       bootstrap,
-      enableDebugging: enableDebugging
-    };
+  function enableDebugging() {
+    debug = true;
   }
 
-  var instance = new SInject();
-  instance.createNew = function() {
-    return new SInject();
+  function getObject(token) {
+    if (debug && bootstrapped) {
+      if (!objMap.hasOwnProperty(token)) {
+        console.warn('Token `' + token + '` was requested, but was never provided.');
+      }
+    }
+    return objMap[token];
+  }
+
+  function provide(token, obj) {
+    if (debug && objMap.hasOwnProperty(token)) {
+      console.warn('Token `' + token + '` is being re-defined, from `' + objMap[token] + '` to `' + obj + '`.');
+    }
+    objMap[token] = obj;
+  }
+
+  function inject(fn/*, tokens */) {
+    var tokens = [].slice.call(arguments, 1);
+    var fnDescription = {tokens: tokens, fn: fn};
+    if (bootstrapped) {
+      performInject(fnDescription);
+    } else {
+      registered.push(fnDescription);
+    }
+  }
+
+  function register(token, obj, fn/*, tokens */) {
+    var tokens = [].slice.call(arguments, 3);
+    provide(token, obj);
+    inject(fn, tokens);
+  }
+
+  function performInject(fnDescription) {
+    fnDescription.fn.apply(undefined, fnDescription.tokens && fnDescription.tokens.map(getObject));
+  }
+
+  function bootstrap() {
+    registered.forEach(performInject);
+    registered = [];
+    bootstrapped = true;
+  }
+
+  return {
+    provide:         provide,
+    inject:          inject,
+    register:        register,
+    bootstrap:       bootstrap,
+    enableDebugging: enableDebugging
   };
+}
 
-  return instance;
-}));
+var instance = new SInject();
+instance.createNew = function() {
+  return new SInject();
+};
+
+export var sinject = instance;
